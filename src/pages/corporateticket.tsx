008e426c93eedf58e56eebe8be2b9569f791123c
szaganek <szaganek@gmail.com>
2025-04-18 16:46:14 +0200

    Open form in new tab

--- src/pages/corporateticket.tsx
+++ src/pages/corporateticket.tsx
@@ -6,16 +6,23 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import CakeStyles from "./index.module.css";
 import styles from "../components/Tickets/styles.module.css";
 
-const AlternativeGoogleForm = 'https://forms.gle/xji9yC834uNVir5j8';
+const AlternativeGoogleForm = "https://forms.gle/xji9yC834uNVir5j8";
+const MailerLiteForm =
+  "https://dashboard.mailerlite.com/forms/1431354/151417944596808868/share";
 
 function TicketForm() {
   return (
     <div className="container">
       <div className={styles.title}>Corporate tickets</div>
-      <div className={styles.spaceBottom20}>Answer these few questions and we'll get back to you.</div>
+      <div className={styles.spaceBottom20}>
+        Answer these few questions and we'll get back to you.
+      </div>
       <div className="ml-embedded" data-form="0lvNEd"></div>
-      <div className="padding-bottom--lg"/>
-      <div className={styles.spaceBottom20}>Can't see the questions? Try refreshing the page or <Link to={AlternativeGoogleForm}>use this form instead.</Link></div>
+      <div className="padding-bottom--lg" />
+      <div className={styles.spaceBottom20}>
+        Can't see the questions? Try refreshing the page or{" "}
+        <Link to={MailerLiteForm}>open the form in a new tab.</Link>
+      </div>
     </div>
   );
 }
