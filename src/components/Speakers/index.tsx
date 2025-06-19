fee02c0bd55f603ba8c28444ffb4e620cc018cd9
szaganek <szaganek@gmail.com>
2025-06-19 13:06:18 +0200

    Add more speakers

--- src/components/Speakers/index.tsx
+++ src/components/Speakers/index.tsx
@@ -14,22 +14,22 @@ type SpeakerItem = {
 
 const SpeakerList: SpeakerItem[] = [
   {
+    picture: "img/speakers/tomek-prus.png",
+    name: "Tomek Prus",
+    desc: "Technical writer, content designer, UX writer",
+    linkedIn: "https://www.linkedin.com/in/tomasz-prus-4b09b01a",
+  },
+  {
+    picture: "img/speakers/wojtek-kutyla.png",
+    name: "Wojtek Kutyła",
+    desc: "Web accessibility specialist, WCAG auditor",
+    linkedIn: "https://www.linkedin.com/in/wojtekkutyla",
+  },
+  {
     picture: "img/speakers/wojtek-aleksander.png",
     name: "Wojtek Aleksander",
     desc: "Content designer, UX writing trainer, published author",
     linkedIn: "https://www.linkedin.com/in/waleksander",
-  },
-  {
-    picture: "img/speakers/pawel-kowaluk.png",
-    name: "Paweł Kowaluk",
-    desc: "ContentOps engineer, podcaster, technical writer",
-    linkedIn: "https://www.linkedin.com/in/pawel-kowaluk",
-  },
-  {
-    picture: "img/speakers/michal-skowron.png",
-    name: "Michał Skowron",
-    desc: "Doc tools developer, podcaster",
-    linkedIn: "https://www.linkedin.com/in/michalskowron",
   },
   {
     picture: "img/speakers/ola-wieczorek.png",
@@ -44,16 +44,28 @@ const SpeakerList: SpeakerItem[] = [
     linkedIn: null,
   },
   {
+    picture: "img/speakers/anna-dulny-leszczynska.png",
+    name: "Anna Dulny-Leszczyńska",
+    desc: "UX designer, workshopper, accessibility advocate",
+    linkedIn: "https://www.linkedin.com/in/anna-dulny-leszczynska",
+  },  
+  {
     picture: "img/speakers/sergio.png",
     name: "Sergio Valero Notari",
     desc: "Content design manager, UX writing teacher",
     linkedIn: "https://www.linkedin.com/in/servalnot/",
   },
   {
-    picture: "img/speakers/anna-dulny-leszczynska.png",
-    name: "Anna Dulny-Leszczyńska",
-    desc: "UX designer, workshopper, accessibility advocate",
-    linkedIn: "https://www.linkedin.com/in/anna-dulny-leszczynska",
+    picture: "img/speakers/pawel-kowaluk.png",
+    name: "Paweł Kowaluk",
+    desc: "ContentOps engineer, podcaster, technical writer",
+    linkedIn: "https://www.linkedin.com/in/pawel-kowaluk",
+  },
+  {
+    picture: "img/speakers/michal-skowron.png",
+    name: "Michał Skowron",
+    desc: "Doc tools developer, podcaster",
+    linkedIn: "https://www.linkedin.com/in/michalskowron",
   },
 ];
 
