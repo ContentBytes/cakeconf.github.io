6c33a933138eff029e9f616e78b24d7cf14e58a8
szaganek <szaganek@gmail.com>
2025-06-21 12:11:23 +0200

    Add section on tracks

--- src/components/Topics/index.tsx
+++ src/components/Topics/index.tsx
@@ -1,19 +1,72 @@
 import clsx from "clsx";
 import styles from "./styles.module.css";
 
+type TopicItem = {
+  icon: string;
+  title: string;
+  desc: string;
+};
+
+const TopicList: TopicItem[] = [
+  {
+    icon: "🎯",
+    title: "Storytelling",
+    desc: "Build your personal brand and sell its unique value",
+  },
+  {
+    icon: "📚",
+    title: "Documentation",
+    desc: "Discover tools, try new approaches, improve your content",
+  },
+  {
+    icon: "📈",
+    title: "Management",
+    desc: "Manage teams, implement strategies, build confidence",
+  },
+  {
+    icon: "🧩",
+    title: "Accessibility",
+    desc: "Make your content available to everyone",
+  },
+  {
+    icon: "✍️",
+    title: "UX writing",
+    desc: "Boost user experience with well-crafted content",
+  },
+  {
+    icon: "🤖",
+    title: "AI",
+    desc: "Transform your content with AI",
+  },
+];
+
+function Topic({ icon, title, desc }: TopicItem) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.icon}>{icon}</div>
+      <div className={styles.cardTitle}>{title}</div>
+      <div className={styles.description}>{desc}</div>
+    </div>
+  );
+}
+
+function CreateTopicCards() {
+  return (
+    <div className={styles.cardsAll}>
+      {TopicList.map((props, idx) => (
+        <Topic key={idx} {...props} />
+      ))}
+    </div>
+  );
+}
 
 export default function Topics() {
   return (
     <div className={styles.background}>
       <div className={clsx("container")}>
-        <div className={clsx(styles.text, styles.title)}>Diverse topics</div>
-        <div className={clsx(styles.text, styles.topics)}>
-          <p>Storytelling</p>
-          <p>Documentation</p>
-          <p>Accessibility</p>
-          <p>UX writing</p>
-          <p>Design</p>
-          <p>AI</p>
+        <div className={clsx(styles.title)}>Thematic tracks</div>
+        <div>
+          <CreateTopicCards />
         </div>
       </div>
     </div>
